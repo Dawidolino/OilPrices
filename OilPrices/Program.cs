dba9e456a3d63286e25510b276c16384d8dac3cb
Dawid Ruth <dawidolinp1@gmail.com>
2023-11-15 20:11:24 +0100

    add dbcontext stuff in program.cs xd

--- OilPrices/Program.cs
+++ OilPrices/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.Options;
+using OilPrices.Model;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext <AppDbContext>(options =>
+options.UseSqlServer()); 
 
 var app = builder.Build();
 
