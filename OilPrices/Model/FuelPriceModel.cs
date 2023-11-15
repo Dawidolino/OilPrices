8e4035621dbb99800fff32146c1a0c77f5efc225
Dawid Ruth <dawidolinp1@gmail.com>
2023-11-15 19:38:25 +0100

    Add project files.

---
+++ OilPrices/Model/FuelPriceModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OilPrices.Model
+{
+    public class FuelPriceModel
+    {
+        [Key]
+        public int FuelPriceModelId { get; set; }
+
+        [ForeignKey("LocationModel")]
+        public int LocationModelId { get; set; }
+        public string FuelType { get; set; } = "";
+        public decimal Price { get; set; } 
+        public DateTime PriceEditDate { get; set; } 
+
+        // one to many relation with location
+        public LocationModel LocationModel { get; set; }
+    }
+
+}
