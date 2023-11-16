e130cc313c2079c286a9bf48cfbd51bc4f00fd1e
Dawid Ruth <dawidolinp1@gmail.com>
2023-11-16 05:14:36 +0100

    changed my model folder to models and also generated the model file from console

---
+++ OilPrices/Models/FuelStation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OilPrices.Models;
+
+public partial class FuelStation
+{
+    public short Id { get; set; }
+
+    public string Nazwa { get; set; } = null!;
+
+    public string? Adres { get; set; }
+
+    public string Miejscowosc { get; set; } = null!;
+
+    public string? KodPocztowy { get; set; }
+
+    public string Wojewodztwo { get; set; } = null!;
+
+    public string Powiat { get; set; } = null!;
+
+    public string? Ron95 { get; set; }
+
+    public string? Ron98 { get; set; }
+
+    public string? On { get; set; }
+
+    public string? Lpg { get; set; }
+}
---
+++ OilPrices/Models/OilPricesAppContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace OilPrices.Models;
+
+public partial class OilPricesAppContext : DbContext
+{
+    public OilPricesAppContext()
+    {
+    }
+
+    public OilPricesAppContext(DbContextOptions<OilPricesAppContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<FuelStation> FuelStations { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-ID0J670;Database=OilPricesApp;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FuelStation>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Adres)
+                .HasMaxLength(100)
+                .HasColumnName("ADRES");
+            entity.Property(e => e.KodPocztowy)
+                .HasMaxLength(50)
+                .HasColumnName("KOD_POCZTOWY");
+            entity.Property(e => e.Lpg)
+                .HasMaxLength(10)
+                .HasColumnName("LPG");
+            entity.Property(e => e.Miejscowosc)
+                .HasMaxLength(50)
+                .HasColumnName("MIEJSCOWOSC");
+            entity.Property(e => e.Nazwa)
+                .HasMaxLength(200)
+                .HasColumnName("NAZWA");
+            entity.Property(e => e.On)
+                .HasMaxLength(10)
+                .HasColumnName("ON");
+            entity.Property(e => e.Powiat)
+                .HasMaxLength(50)
+                .HasColumnName("POWIAT");
+            entity.Property(e => e.Ron95)
+                .HasMaxLength(10)
+                .HasColumnName("RON95");
+            entity.Property(e => e.Ron98)
+                .HasMaxLength(10)
+                .HasColumnName("RON98");
+            entity.Property(e => e.Wojewodztwo)
+                .HasMaxLength(50)
+                .HasColumnName("WOJEWODZTWO");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
