fdaa803585550a01a76e2c177f24733c2f658c22
Dawid Ruth <dawidolinp1@gmail.com>
2023-11-16 02:18:33 +0100

    cleaned up

--- OilPrices/Program.cs
+++ OilPrices/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext <AppDbContext>(options =>
-options.UseSqlServer()); 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
 
 var app = builder.Build();
 
