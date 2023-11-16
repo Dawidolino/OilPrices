5c143ac0ef6b07cdecf12717254f1ed42a4e2112
Dawid Ruth <dawidolinp1@gmail.com>
2023-11-16 06:05:11 +0100

    comment

--- OilPrices/Program.cs
+++ OilPrices/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.Extensions.Options;
-using OilPrices.Model;
+using OilPrices.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,8 +11,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext <AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+//builder.Services.AddDbContext <AppDbContext>(options =>
+//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
 
 var app = builder.Build();
 
