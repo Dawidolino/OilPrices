d0be6cf5e3e3e7fb229c6631d6377ae5e48f554e
Dawid Ruth <dawidolinp1@gmail.com>
2024-01-18 15:23:15 +0100

    there are bugs in fuel station form component but im changing the project

--- OilPrices/src/app/fuel-station/fuel-station-form/fuel-station-form.component.ts
+++ OilPrices/src/app/fuel-station/fuel-station-form/fuel-station-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, NgModule } from '@angular/core';
 import { FuelStationService } from '../../shared/fuel-station.service';
 import { FormsModule } from '@angular/forms';
+import { FuelPricesService } from '../../shared/fuel-prices.service';
+import { FuelPrices } from '../../shared/fuel-prices.model';
 
 @Component({
   selector: 'app-fuel-station-form',
@@ -11,7 +13,27 @@ import { FormsModule } from '@angular/forms';
 })
 export class FuelStationFormComponent {
 
-  constructor(public service: FuelStationService){
+  constructor(public service: FuelStationService, public priceService: FuelPricesService) {}
+  ngOnInit(): void {
+   
+    // Fetch fuel prices
+    this.priceService.getFuelPrices().subscribe(prices => {
+      const fuelPricesForStation = prices.find(price => price.fuelStationId === FuelPrices.fuelStationId);
 
+      if (fuelPricesForStation) {
+        // Fetch fuel station details based on the foreign key (fuelStationId)
+        this.service.getFuelStations().subscribe(stations => {
+          this.service.formData = stations.find(station=>station.id===FuelPricesService.fuelStationId);
+        });
+
+        // Assign fuel prices to the form data
+        this.service.formData.ron95 = fuelPricesForStation.roN95;
+        this.service.formData.ron98 = fuelPricesForStation.roN98;
+        this.service.formData.on = fuelPricesForStation.on;
+        this.service.formData.lpg = fuelPricesForStation.lpg;
+      }
+    });
   }
+  
 }
+``
