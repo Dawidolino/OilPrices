8982fdf76b38922f5fbb66db61dfe7db23b7dba6
Dawid Ruth <dawidolinp1@gmail.com>
2024-01-18 15:43:14 +0100

    remove fuel-prices from the project

--- OilPrices/Models/OilPricesAppContext.cs
+++ OilPrices/Models/OilPricesAppContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +16,7 @@ public partial class OilPricesAppContext : DbContext
     }
 
     public virtual DbSet<FuelStation> FuelStations { get; set; }
-    public DbSet<FuelPrices> FuelPrices { get; set; }
+    //public DbSet<FuelPrices> FuelPrices { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
--- OilPrices/src/app/fuel-station/fuel-station.component.ts
+++ OilPrices/src/app/fuel-station/fuel-station.component.ts
@@ -6,47 +6,24 @@ import { NgIf } from '@angular/common';
 import { AsyncPipe } from '@angular/common';
 import { Observable,  combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { FuelPricesService } from '../shared/fuel-prices.service';
+
 @Component({
     selector: 'app-fuel-station',
     standalone: true,
     templateUrl: './fuel-station.component.html',
     styles: ``,
     imports: [FuelStationFormComponent,AsyncPipe ,NgFor, NgIf], 
-    providers: [FuelStationService, FuelPricesService] 
+    providers: [FuelStationService] 
 })
 
 export class FuelStationComponent implements OnInit {
   
     fuelData$!: Observable<any>;
   
-    constructor(
-      public service: FuelStationService, 
-      public priceService :FuelPricesService){}
+    constructor( public service: FuelStationService){}
      ngOnInit(): void {
-        this.fuelData$ = combineLatest([
-            this.service.getFuelStations(),
-            this.priceService.getFuelPrices()
-          ]).pipe(
-            map(([fuelStations, fuelPrices]) => {
-              console.log('Fuel Stations:', fuelStations);
-              console.log('Fuel Prices:', fuelPrices);
-          
-              const mappedData = fuelStations.map(station => {
-                const pricesForStation = fuelPrices.find(price => price.fuelStationId === station.id);
-                const mappedStation = {
-                  ...station,
-                  prices: pricesForStation || { roN95: '0', roN98: '0', on: '0', lpg: '0' }
-                };
-                console.log('Mapped Station:', mappedStation);
-                return mappedStation;
-              });
-          
-              console.log('Mapped Data:', mappedData);
-          
-              return mappedData;
-            })
-          );
+      
+         
 
     }
   
